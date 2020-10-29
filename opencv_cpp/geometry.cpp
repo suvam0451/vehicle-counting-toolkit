cd640199811880bcecd8dc6535ed449226bd48c2
Debashish Patra <suvam@localhost.localdomain>
2020-10-29 16:59:40 +0530

    Adding line/segment/point classes

--- opencv_cpp/geometry.cpp
+++ opencv_cpp/geometry.cpp
@@ -1,8 +1,10 @@
-
+// OK
 
 namespace geometry {
 
+/* To represent a point on the image */
 class Point {
+   public:
     int X;
     int Y;
 
@@ -14,12 +16,38 @@ class Point {
 
 class Line {
    public:
-    int X;
     int C;
     int M;
+
+    Line() {}
+
+    Line(Point A, Point B) {
+        M = (B.Y - A.Y) / (B.X - A.X);
+        C = B.Y - M * B.X;
+    }
+
+    int RelativePositionToLine(Point A);
 };
 
-static int PositionRelativeToLine() {
-    return 0;
+int Line::RelativePositionToLine(Point A) {
+    if (A.Y == A.X * M + C)
+        return 0;
+    if (A.Y > A.X * M + C)
+        return 1;
+
+    return -1;
 }
+
+class Segment : Line {
+    Point start;
+    Point end;
+
+    Segment(Point A, Point B) {
+        start = A;
+        end = B;
+        M = (B.Y - A.Y) / (B.X - A.X);
+        C = B.Y - M * B.X;
+    }
+};
+
 }  // namespace geometry
